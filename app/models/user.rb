f53d78594f7a918e0988883e6211346b0f3ebb4a
takanotakuya <tmw9898@icloud.com>
2021-02-19 20:20:21 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+  validates :nickname
+  validates :first_name
+  validates :second_name
+  validates :first_name_kana
+  validates :second_name_kana
+  validates :birthday
+  end
 end
