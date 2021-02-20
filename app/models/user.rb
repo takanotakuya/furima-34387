ed62d0138f880d8b8f724662de8bb59a90728883
takanotakuya <tmw9898@icloud.com>
2021-02-20 12:58:03 +0900

    バリデーション　詳細設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,12 +4,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+
+  with_options presence: true do
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
+  end
+
   with_options presence: true do
   validates :nickname
-  validates :first_name
-  validates :second_name
-  validates :first_name_kana
-  validates :second_name_kana
   validates :birthday
+  end
+  
+  with_options presence: true do
+  validates_format_of :first_name, with: /\A[一-龥]+\z/, message: 'には全角漢字で入力してください'
+  validates_format_of :second_name, with: /\A[一-龥]+\z/, message: 'には全角漢字で入力してください'
+  end
+
+  with_options presence: true do
+  validates_format_of :first_name_kana, with: /\A[ァ-ヶー－]+\z/, message: 'には全角カナで入力してください'
+  validates_format_of :second_name_kana, with: /\A[ァ-ヶー－]+\z/, message: 'には全角カナで入力してください'
   end
 end
