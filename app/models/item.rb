c7692c2be2ff7ba2cd9019c8df55be4a98483001
takanotakuya <tmw9898@icloud.com>
2021-02-21 12:11:18 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,6 +3,17 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one_attached :image
 
+  with_options presence: true do
+  validates :name
+  validates :description
+  validates :category_id, numericality: { other_than: 1 }
+  validates :status_id, numericality: { other_than: 1 }
+  validates :charge_burden_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :price
+  validates :day_id, numericality: { other_than: 1 }
+  end
+
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :category
   belongs_to :status
