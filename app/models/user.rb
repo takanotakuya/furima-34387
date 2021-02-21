29ef9bfed50e3ccb9b286e3f35d073dfd585e029
takanotakuya <tmw9898@icloud.com>
2021-02-21 11:08:57 +0900

    モデル・テーブル作成、アソシエーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,9 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :items
+  has_many :orders
+
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
 
   with_options presence: true do
