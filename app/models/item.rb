4834a05c0e44a2d736467bf3ac03ba42c4a359e9
takanotakuya <tmw9898@icloud.com>
2021-02-21 12:02:40 +0900

    ActiveHash詳細記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,11 @@ class Item < ApplicationRecord
   has_one    :order
   belongs_to :user
   has_one_attached :image
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :status
+  belongs_to :charge_burden
+  belongs_to :prefecture
+  belongs_to :day
 end
