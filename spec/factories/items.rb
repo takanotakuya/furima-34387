a5a3c36cca6a6e5e48a01ab36f7d0be1d61fef1f
takanotakuya <tmw9898@icloud.com>
2021-02-22 18:10:14 +0900

    テストコード記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
-    
+    name {"商品名"}
+    description {"説明文"}
+    category_id {2}
+    status_id {2}
+    charge_burden_id {2}
+    prefecture_id {2}
+    price {3000}
+    day_id {2}
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
