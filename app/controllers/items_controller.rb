780ab5194dfa6347fec7e8e68bd777c93e72a52c
takanotakuya <tmw9898@icloud.com>
2021-02-23 18:45:12 +0900

    レビュー修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -23,15 +23,12 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if @item.user == current_user
-      render "edit"
-    else
-      redirect_to root_path
-    end
+    redirect_to root_path if @item.user != current_user
   end
 
   def update
-    if @item.update(item_params)
+    if @item.user == current_user
+      @item.update(item_params)
       redirect_to item_path(@item.id)
     else
       render :edit
