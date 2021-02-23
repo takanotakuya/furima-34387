0f9be6a2e6a409dae036576354775af6e794f54e
takanotakuya <tmw9898@icloud.com>
2021-02-23 20:23:16 +0900

    レビュー修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, excepet: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:edit, :show, :update, :destroy]
   before_action :sets_item, only: [:edit, :update, :destroy]
 
